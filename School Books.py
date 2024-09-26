cfc1d05afed04930507b9e86f40f3c6935f0b9e8
Assiddiq11 <abubakarmahmoudmaina@gmail.com>
2024-09-26 18:20:37 +0100

    Add files via upload

---
+++ School Books.py
@@ -0,0 +1,12 @@
+dictionary = {
+    "English Book":"£5",
+    "Maths Book":"£5",
+    "Geography Book":"£5",
+    "History Book":"£5",
+    "Science Book":"£5"
+}
+
+print (dictionary)
+
+print (dictionary["English Book"])
+print (dictionary)
